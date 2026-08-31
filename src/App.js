5b78ab8fc76dd8bf8929c6c581b6e8cebd5ef24a
y4e1-png <y4e1.aguilar@icloud.com>
2026-08-31 17:17:44 -0600

    Crea biblioteca musical con componentes de clase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Header from './components/Header';
+import Song from './components/Song';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  componentDidMount() {
+    console.log("La aplicación se ha montado correctamente.");
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <main className="biblioteca">
+          <h2>Mi playlist</h2>
+          <Song
+            title="Neón Sobre Tlalpan"
+            artist="Marea de Bolsillo"
+            album="Kilometro Cero"
+            duration="3:19"
+          />
+          <Song
+            title="La planta del departamento 6"
+            artist="Abril Magnética"
+            album="Objetos que sobreviven a una mudanza"
+            duration="3:45"
+          />
+          <Song
+            title="Café a las 2:17"
+            artist="Los Edificios Vacíos"
+            album="Manual para perder el último metro"
+            duration="3:51"
+          />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="header">
+                <h1>Biblioteca Musical</h1>
+            </header>
+        );
+    }
+}
+export default Header;
